f5ef1cfd5c56b425ec05829dd6b78cf69c0f3c78
Tyler Puleo <tyler.puleo22@gmail.com>
2015-02-13 14:11:50 -0500

    whitespace

--- ExampleCode/HTMLandJS/apiExample/example.js
+++ ExampleCode/HTMLandJS/apiExample/example.js
@@ -1,5 +1,5 @@
 function run() {
-/*
+
 	FINISHED AND WORKING
 
 	/api/v1/projects/XX
@@ -15,8 +15,7 @@ function run() {
 
 
     console.log(parsedResponseProject);
-*/
-/*
+
 	FINISHED AND WORKING
 
 	/api/v1/projects
@@ -32,8 +31,7 @@ function run() {
 
 
     console.log(parsedResponseProject);
-*/
-/*
+
 	FINISHED AND WORKING
 	
 	/api/v1/projects
@@ -47,9 +45,7 @@ function run() {
         'project_name': 'TEST123456',
     }
     $.post(apiUrl, upload);
-*/
 
-/*
 	FINISHED AND WORKING
 
 	/api/v1/projects/XX/add_key
@@ -66,8 +62,8 @@ function run() {
         			   }
     }
     $.post(apiUrl, upload);
-*/
-/*
+
+
 	FINISHED AND WORKING
 
 	/api/v1/fields
@@ -86,8 +82,8 @@ function run() {
         		  }
     }
     $.post(apiUrl, upload);
-*/
-/*
+
+
 	FINISHED AND WORKING
 
 	/api/v1/fields/XX
@@ -103,8 +99,8 @@ function run() {
 
 
     console.log(parsedResponseProject);
-*/
-/*
+
+
 	FINISHED AND WORKING
 
 	/api/v1/projects/XX/jsonDataUpload
@@ -121,8 +117,8 @@ function run() {
         		 }
     }
     $.post(apiUrl, upload);
-*/
-/*
+
+
 	FINISHED AND WORKING
 
 	/api/v1/projects/XX/jsonDataUpload
@@ -142,8 +138,8 @@ function run() {
     $.post(apiUrl, upload);
 
 
-*/
-/*
+
+
 
 	/api/v1/data_sets/XX/edit
 
@@ -164,8 +160,8 @@ function run() {
     }
     $.post(apiUrl, upload);
 
-*/
-/*
+
+
 	/api/v1/data_sets/XX/edit
 
 	API SAYS GET BUT SET UP LIKE POST
@@ -183,8 +179,8 @@ function run() {
         		 }
     }
     $.post(apiUrl, upload);
-*/
-/*
+
+
 	/api/v1/data_sets/append
 
 	FINISHED AND WORKING
@@ -202,8 +198,8 @@ function run() {
         		 }
     }
     $.post(apiUrl, upload);
-*/
-/*
+
+
 	/api/v1/data_sets/append
 
 	FINISHED AND WORKING
@@ -220,16 +216,14 @@ function run() {
         		 }
     }
     $.post(apiUrl, upload);
-*/
 
-/*
-	DONT KNOW HOW TO DO MEDIA OBJECTS
-*/
 
-/*
+
 	DONT KNOW HOW TO DO MEDIA OBJECTS
-*/
-/*
+
+	DONT KNOW HOW TO DO MEDIA OBJECTS
+
+
 	NOT WORKING DONT KNOW HOW TO PASS USERNAME OR PASSWORD, ASSUMING IN URL?
 
     var urlProject = 'http://isenseproject.org/api/v1/users/t@t.tt';
@@ -241,163 +235,8 @@ function run() {
 
     var parsedResponseProject = JSON.parse(responseProject); 
 
-
     console.log(parsedResponseProject);
 
-
     console.log("POST SUCCESSFUL");
-*/
+
 }
-
-
-
-
-
-
-
-
-
-
-
-"use strict";
-window.isense = {};
-var baseUrl = 'http://rsense-dev.cs.uml.edu/api/v1/';
-
-var isense = {
-
-    isense : function(projectID,contributorKey,contributorName){
-
-        this.projectID = projectID;
-        this.contributorKey = contributorKey;
-        this.contributorName = contributorName;
-    },
-
-
-    projectGetRequest : function() {
-
-        var urlProject = baseUrl+ 'projects/' + this.projectID+'?recur=true';
-        var responseProject = $.ajax({ type: "GET",
-                            url: urlProject,
-                            async: false,
-                            dataType: "JSON"
-        }).responseText;
-
-        var parsedResponseProject = JSON.parse(responseProject); 
-
-        return parsedResponseProject;  
-    },
-
-    getDatasetLocation : function(datasetName,parsedResponseProject) {
-
-        for (var j = 0; j < parsedResponseProject.dataSetCount; j++) { 
-
-            if (parsedResponseProject.dataSets[j].name == datasetName) {
-
-                var datasetLocation = j;
-                var datasetID = parsedResponseProject.dataSets[j].id;
-            }
-        }
-
-        if (datasetID == null) {
-
-            return "Dataset Not Found"
-        }
-        return datasetLocation;
-    },
-
-    getFieldID : function(fieldName,parsedResponseProject) {
-
-        for (var i = 0; i < parsedResponseProject.fieldCount; i++) {    // Parsing through fields looking for field id
-
-            if (parsedResponseProject.fields[i].name == fieldName) {     // If field names match then this is the id
-
-                var fieldID = parsedResponseProject.fields[i].id;       // This is the field ID
-            }
-        }
-
-        if (fieldID == null) {
-
-            return "Field Not Found"
-        }
-
-        fieldID = fieldID.toString();
-
-        return fieldID;
-    },
-
-    getDatasetFieldData : function(datasetName,fieldName) {
-
-        var values = [];
-        var parsedResponseProject = isense.projectGetRequest(this.projectID);
-        var datasetLocation = isense.getDatasetLocation(datasetName,parsedResponseProject);
-        var fieldID = isense.getFieldID(fieldName,parsedResponseProject);
-
-        for (var k = 0; k < parsedResponseProject.dataSets[datasetLocation].datapointCount; k++) {
-
-            values.push(parsedResponseProject.dataSets[datasetLocation].data[k][fieldID])   
-        }
-
-        return values;
-    },
-
-    postDataset : function(fieldName,title,data) {
-
-        var currentTime = new Date();
-        var timestamp = JSON.stringify(currentTime);
-        var parsedResponseProject = isense.projectGetRequest(this.projectID);
-        var fieldID = isense.getFieldID(fieldName,parsedResponseProject);
-        var fieldIDString = fieldID.toString();
-        var dataForPost = {};
-        dataForPost[fieldIDString] = data;
-        var apiUrl = baseUrl+ 'projects/' + this.projectID+'/jsonDataUpload';
-
-        var upload = {
-
-            'title': title + ' ' + timestamp,
-            'contribution_key': this.contributorKey,
-            'contributor_name': this.contributorName,
-            'data': dataForPost
-        }
-        $.post(apiUrl, upload);
-        alert("Post Successful");
-    },
-
-    postDatasetHorizontal : function(fields,title,data) {
-
-        var currentTime = new Date();
-        var timestamp = JSON.stringify(currentTime);
-        var parsedResponseProject = isense.projectGetRequest(this.projectID);
-        var dataForPost = {};
-        var fieldID = []
-
-        for (var i = 0; i < fields.length; i++) {
-
-            fieldID[i] = isense.getFieldID(fields[i],parsedResponseProject);
-            dataForPost[fieldID[i]] = data[i];
-        };
-
-        var apiUrl = baseUrl+ 'projects/' + this.projectID+'/jsonDataUpload';
-        var upload = {
-
-            'title': title + ' ' + timestamp,
-            'contribution_key': this.contributorKey,
-            'contributor_name': this.contributorName,
-            'data': dataForPost
-        }
-        $.post(apiUrl, upload);
-        alert("Post Successful");
-    },
-
-    appendToDataset : function(datasetName,fields,data) {
-
-        var apiUrl =' http://rsense-dev.cs.uml.edu/api/v1/data_sets/append';
-        var upload = {
-
-            'email': 'isenseproject@gmail.com',
-            'password': 'fieilds',
-            'id': 7736,
-            'data': {'638':[4564,5,4,8,7,87,8,4,84]}
-        }
-        $.post(apiUrl, upload);
-    }
-};
